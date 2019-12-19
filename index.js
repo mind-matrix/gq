8abf2d4e4584a2d87e024316f458de7259ded2a6
Sagnik Modak <sagnikmodak1118@gmail.com>
2019-12-20 02:10:46 +0530

    Fixed module path problem

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #! /usr/bin/env node
 const program = require('commander');
 const fs = require('fs');
+const path = require('path');
 
-const modules = fs.readdirSync('./modules');
+const modules = fs.readdirSync(path.join(__dirname, './modules'));
 modules.forEach((mod) => {
-    var manifest = JSON.parse(fs.readFileSync(`./modules/${mod}/manifest.json`, { encoding: 'utf-8' }));
-    program.command(manifest.command, manifest.description, { executableFile: `./modules/${mod}/index.js` }).alias(manifest.alias);
+    var manifest = JSON.parse(fs.readFileSync(path.join(__dirname,`./modules/${mod}/manifest.json`), { encoding: 'utf-8' }));
+    program.command(manifest.command, manifest.description, { executableFile: path.join(__dirname, `./modules/${mod}/index.js`) }).alias(manifest.alias);
 });
 program.parse(process.argv);
